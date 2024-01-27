69eb54e24a1c01072acf2cc94da35c49085954ad
NewellNweke <156318911+NewellNweke@users.noreply.github.com>
2024-01-27 11:48:18 +0100

    added an api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+function searchCity(city){
+    let apiKey = "o494a3dd60ae3ea54b6f5fbebb05t362";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}& units=metric`;
+    console.log(apiUrl);
+}
+
+
+
+
+
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#weather-city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
