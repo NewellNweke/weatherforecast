f782126fd72272585b3f93c1157bbce20f70d210
NewellNweke <156318911+NewellNweke@users.noreply.github.com>
2024-02-05 12:11:40 +0100

    updated the js

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ function refreshWeather(response){
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML= Math.round(temperature);
+
+  getForecast(response.data.city);
 }
 function formatDate(date){
    let minutes = date.getMinutes();
@@ -93,4 +95,3 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
-getForecast("Paris");
