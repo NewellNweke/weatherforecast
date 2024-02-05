9cb8cc2c7c29f4418f3531d3c96a2d611ee6593a
NewellNweke <156318911+NewellNweke@users.noreply.github.com>
2024-02-05 10:21:33 +0100

    updated the java

--- src/index.js
+++ src/index.js
@@ -51,82 +51,19 @@ function handleSearchSubmit(event){
     let searchInput = document.querySelector("#search-form-input");
        searchCity(searchInput.value);
 }
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
+function displayForecast() {
+    let forecast = document.querySelector("#forecast");
 
-searchCity("Paris");
+    let days = ["Tues", "Wed", "Thurs", "Fri", "Sat"];
+    let forecastHtml = "";
 
-let forecast = document.querySelector("#forecast");
-
-forecast.innerHTML = `
+    days.forEach(function(day) {
+        forecastHtml= forecastHtml +
+        `
     <div class="weather-forecast-day">
     <div class ="col-2">
         <div class="weather-forecast-date">
-            Tues
-        </div>
-        <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/shower-rain-night.png" alt="weather" width ="42"/>
-        <div class="weather-forecast-temperature">
-            <span class="weather-forecast-temperature-max">
-               <strong>18&deg;</strong>
-            </span>
-                <span class= "weather-forecast-temperature-min">
-                   <strong>12&deg;</strong> 
-                </span>
-        </div>
-    </div>
-</div>
-    <div class="weather-forecast-day">
-    <div class ="col-2">
-        <div class="weather-forecast-date">
-            Wed
-        </div>
-        <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/shower-rain-night.png" alt="weather" width ="42"/>
-        <div class="weather-forecast-temperature">
-            <span class="weather-forecast-temperature-max">
-               <strong>18&deg;</strong>
-            </span>
-                <span class= "weather-forecast-temperature-min">
-                   <strong>12&deg;</strong> 
-                </span>
-        </div>
-    </div>
-</div>
-    <div class="weather-forecast-day">
-    <div class ="col-2">
-        <div class="weather-forecast-date">
-            Thurs
-        </div>
-        <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/shower-rain-night.png" alt="weather" width ="42"/>
-        <div class="weather-forecast-temperature">
-            <span class="weather-forecast-temperature-max">
-               <strong>18&deg;</strong>
-            </span>
-                <span class= "weather-forecast-temperature-min">
-                   <strong>12&deg;</strong> 
-                </span>
-        </div>
-    </div>
-</div>
-    <div class="weather-forecast-day">
-    <div class ="col-2">
-        <div class="weather-forecast-date">
-            Fri
-        </div>
-        <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/shower-rain-night.png" alt="weather" width ="42"/>
-        <div class="weather-forecast-temperature">
-            <span class="weather-forecast-temperature-max">
-               <strong>18&deg;</strong>
-            </span>
-                <span class= "weather-forecast-temperature-min">
-                   <strong>12&deg;</strong> 
-                </span>
-        </div>
-    </div>
-</div>
-    <div class="weather-forecast-day">
-    <div class ="col-2">
-        <div class="weather-forecast-date">
-            Sat
+            ${day}
         </div>
         <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/shower-rain-night.png" alt="weather" width ="42"/>
         <div class="weather-forecast-temperature">
@@ -140,3 +77,13 @@ forecast.innerHTML = `
     </div>
 </div>
 `;
+});
+
+forecastElement.innerHtml=forecastHtml;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
+displayForecast();
